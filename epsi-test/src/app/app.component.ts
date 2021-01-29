51a63a503131e87108c88f6fb32e4ec1483ea3ec
pavanraj <pavanraj.sn@candelalabs.io>
2021-01-29 20:18:49 +0530

    cganges

--- epsi-test/src/app/app.component.ts
+++ epsi-test/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit{
+
+    endPoint : string = "http://usweb.dotomi.com/resources/swfs/cookies.json";
+    data : any = [];
+
+    constructor(private http: HttpClient) {
+    }
+
+    ngOnInit(){
+      this.http.get(this.endPoint).subscribe((data)=>{
+        this.data = data;
+      });
+    }
+
 }
--- epsi-test/src/app/app.module.ts
+++ epsi-test/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DataTableModule } from "angular-6-datatable";
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DataTableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
